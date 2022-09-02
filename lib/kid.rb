2eaffe9c0db376b51add390013a5f3e389d56513
Kelvin <vokemalosh32@gmail.com>
2022-09-02 11:21:21 +0300

    commit tree d5f427f00003e39e96a8dff87585b9c1e32a065a

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,9 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    def initialize(name)
+        @name = name
+      end
+end
